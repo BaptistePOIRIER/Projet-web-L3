4239d610159bd961f9f0fb3a18f31ce12ca6bd7c
Theticman <41964761+Theticman@users.noreply.github.com>
2021-01-07 18:16:04 +0100

    Contribute start

--- client/components/Contribute.vue
+++ client/components/Contribute.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Contribute page</h1>
+    <div>
+        <header-tpl :connected="connected"></header-tpl>
+        <h1>Contribute page</h1>
+        <input type="text" v-model="this.newDefinition.word">
+        <input type="text" v-model="this.newDefinition.definition">
+        <button>Submit</button>
+    </div>
 </template>
+
+<script>
+  },
+  components: {
+    'header-tpl': HeaderTemplate
+  },
+  data () {
+      return {
+          newDefinition: {
+              word: "",
+              definition: ""
+          }
+      }
+  },
+  mounted() {
+    if (!this.connected) {
+      router.push('/login')
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif;
+}
+</style>
